d2d09ffa09fc6c638dd026943399f1fe901e241e
Ravind <iamurs28@gmail.com>
2020-06-27 16:07:26 +0530

    my first commit

---
+++ src/main/java/com/qa/utility/TestUtility.java
@@ -0,0 +1,9 @@
+package com.qa.utility;
+
+import com.qa.base.TestBase;
+
+public class TestUtility extends TestBase{
+
+	public static long ImplicitWait = 200;
+	public static long PageLoadTimeout = 200;
+}
